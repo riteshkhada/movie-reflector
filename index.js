6c9b613c2362dabd57c6990ef3b2a7ad3f137dc6
riteshkhadka <khadkaritesh45@gmail.com>
2021-07-22 02:02:03 +0545

    initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const modal = document.querySelector (".modal");
+const loginBtn = document.querySelector (".btn-login");
+const btnClose = document.querySelector (".closeImg");
+
+
+loginBtn.addEventListener ("click", openModal);
+btnClose.addEventListener ("click", closeModal);
+window.addEventListener ("click", clickOut);
+
+function openModal () {
+    modal.style.display = "block";
+};
+function closeModal () {
+    modal.style.display = "none";
+};
+function clickOut (e) {
+    if (e.target == modal) {
+        closeModal ();
+    }};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
